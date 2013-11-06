344d3f1a41f864c5a4d6c4be3fdd04d915531744
Guillaume Claret <dev@clarus.me>
2013-11-06 15:03:52 +0100

    Parsing and printing

--- coqOfOCaml.ml
+++ coqOfOCaml.ml
@@ -1,8 +1,15 @@
 let parse_and_print file_name =
-  Printf.printf "Parsing %s ...\n" file_name;
+  Printf.printf "Parsing %s:\n" file_name;
+  let env = Env.initial in
   let input = Pparse.preprocess file_name in
-  let structure = Pparse.file Format.str_formatter input Parse.implementation Config.ast_impl_magic_number in
-  ()
+  let input = Pparse.file Format.str_formatter input Parse.implementation Config.ast_impl_magic_number in
+  let (structure, signature, env) = Typemod.type_toplevel_phrase env input in
+  Printtyp.signature Format.str_formatter signature;
+  let signature = Format.flush_str_formatter () in
+  Printtyped.implementation Format.str_formatter structure;
+  let structure = Format.flush_str_formatter () in
+  print_endline signature;
+  print_endline structure
 
 let main () =
   Arg.parse [] parse_and_print "Usage: ..."
