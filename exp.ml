68cf0e0f20bd3e2d9a726ab4cde53f77149e072c
Guillaume Claret <dev@clarus.me>
2014-02-20 14:31:34 +0100

    Beginning of a generic environment and annotated expressions

--- exp.ml
+++ exp.ml
@@ -24,87 +24,112 @@ module Header = struct
 end
 
 (** The simplified OCaml AST we use. *)
-type t =
-  | Constant of Constant.t
-  | Variable of PathName.t
-  | Tuple of t list (** A tuple of expressions. *)
-  | Constructor of PathName.t * t list (** A constructor name and a list of arguments. *)
-  | Apply of t * t (** An application. *)
-  | Function of Name.t * t (** An argument name and a body. *)
-  | Let of Header.t * t * t
-  | Match of t * (Pattern.t * t) list (** Match an expression to a list of patterns. *)
-  | Record of (PathName.t * t) list (** Construct a record giving an expression for each field. *)
-  | Field of t * PathName.t (** Access to a field of a record. *)
-  | IfThenElse of t * t * t (** The "else" part may be unit. *)
-  | Sequence of t * t (** A sequence of two expressions. *)
-  | Return of t (** Monadic return. *)
-  | Bind of t * Name.t option * t (** Monadic bind. *)
-  | Lift of Effect.Descriptor.t * Effect.Descriptor.t * t (** Monadic lift. *)
+type 'a t =
+  | Constant of 'a * Constant.t
+  | Variable of 'a * PathName.t
+  | Tuple of 'a * 'a t list (** A tuple of expressions. *)
+  | Constructor of 'a * PathName.t * 'a t list (** A constructor name and a list of arguments. *)
+  | Apply of 'a * 'a t * 'a t (** An application. *)
+  | Function of 'a * Name.t * 'a t (** An argument name and a body. *)
+  | Let of 'a * Header.t * 'a t * 'a t
+  | Match of 'a * 'a t * (Pattern.t * 'a t) list (** Match an expression to a list of patterns. *)
+  | Record of 'a * (PathName.t * 'a t) list (** Construct a record giving an expression for each field. *)
+  | Field of 'a * 'a t * PathName.t (** Access to a field of a record. *)
+  | IfThenElse of 'a * 'a t * 'a t * 'a t (** The "else" part may be unit. *)
+  | Sequence of 'a * 'a t * 'a t (** A sequence of two expressions. *)
+  | Return of 'a * 'a t (** Monadic return. *)
+  | Bind of 'a * 'a t * Name.t option * 'a t (** Monadic bind. *)
+  | Lift of 'a * Effect.Descriptor.t * Effect.Descriptor.t * 'a t (** Monadic lift. *)
 
-let rec pp (e : t) : SmartPrint.t =
+let rec clean (e : 'a t) : unit t =
   match e with
-  | Constant c -> Constant.pp c
-  | Variable x -> PathName.pp x
-  | Tuple es -> nest (!^ "Tuple" ^^ Pp.list (List.map pp es))
-  | Constructor (x, es) ->
-    nest (!^ "Constructor" ^^ Pp.list (PathName.pp x :: List.map pp es))
-  | Apply (e_f, e_x) ->
-    nest (!^ "Apply" ^^ Pp.list [pp e_f; pp e_x])
-  | Function (x, e) ->
-    nest (!^ "Function" ^^ Pp.list [Name.pp x; pp e])
-  | Let (header, e1, e2) ->
-    nest (!^ "Let" ^^ Header.pp header ^^ !^ "=" ^^ newline ^^
+  | Constant (_, c) -> Constant ((), c)
+  | Variable (_, x) -> Variable ((), x)
+  | Tuple (_, es) -> Tuple ((), List.map clean es)
+  | Constructor (_, x, es) -> Constructor ((), x, List.map clean es)
+  | Apply (_, e_f, e_x) -> Apply ((), clean e_f, clean e_x)
+  | Function (_, x, e) -> Function ((), x, clean e)
+  | Let (_, header, e1, e2) -> Let ((), header, clean e1, clean e2)
+  | Match (_, e, cases) ->
+    Match ((), clean e, List.map (fun (p, e) -> (p, clean e)) cases)
+  | Record (_, fields) ->
+    Record ((), List.map (fun (x, e) -> (x, clean e)) fields)
+  | Field (_, e, x) -> Field ((), clean e, x)
+  | IfThenElse (_, e1, e2, e3) -> IfThenElse ((), clean e1, clean e2, clean e3)
+  | Sequence (_, e1, e2) -> Sequence ((), clean e1, clean e2)
+  | Return (_, e) -> Return ((), clean e)
+  | Bind (_, e1, x, e2) -> Bind ((), clean e1, x, clean e2)
+  | Lift (_, d1, d2, e) -> Lift ((), d1, d2, clean e)
+
+let rec pp (pp_a : 'a -> SmartPrint.t) (e : 'a t) : SmartPrint.t =
+  let pp = pp pp_a in
+  match e with
+  | Constant (a, c) -> nest (!^ "Constant" ^^ Pp.list [pp_a a; Constant.pp c])
+  | Variable (a, x) -> nest (!^ "Variable" ^^ Pp.list [pp_a a; PathName.pp x])
+  | Tuple (a, es) ->
+    nest (!^ "Tuple" ^^ Pp.list (pp_a a :: List.map pp es))
+  | Constructor (a, x, es) ->
+    nest (!^ "Constructor" ^^ Pp.list (pp_a a :: PathName.pp x :: List.map pp es))
+  | Apply (a, e_f, e_x) ->
+    nest (!^ "Apply" ^^ Pp.list [pp_a a; pp e_f; pp e_x])
+  | Function (a, x, e) ->
+    nest (!^ "Function" ^^ Pp.list [pp_a a; Name.pp x; pp e])
+  | Let (a, header, e1, e2) ->
+    nest (!^ "Let" ^^ pp_a a ^^ Header.pp header ^^ !^ "=" ^^ newline ^^
       indent (pp e1) ^^ !^ "in" ^^ newline ^^
       pp e2)
-  | Match (e, cases) ->
-    nest (!^ "Match" ^^ Pp.list [pp e; cases |> OCaml.list (fun (p, e) ->
-      nest @@ parens (Pattern.pp p ^-^ !^ "," ^^ pp e))])
-  | Record fields ->
-    nest (!^ "Record" ^^ Pp.list (fields |> List.map (fun (x, e) ->
-      nest @@ parens (PathName.pp x ^-^ !^ "," ^^ pp e))))
-  | Field (e, x) -> nest (!^ "Field" ^^ Pp.list [pp e; PathName.pp x])
-  | IfThenElse (e1, e2, e3) ->
-    nest (!^ "IfThenElse" ^^ Pp.list [pp e1; pp e2; pp e3])
-  | Sequence (e1, e2) -> nest (!^ "Sequence" ^^ Pp.list [pp e1; pp e2])
-  | Return e -> nest (!^ "Return" ^^ Pp.list [pp e])
-  | Bind (e1, x, e2) -> nest (!^ "Bind" ^^ Pp.list
-    [pp e1; nest (OCaml.option Name.pp x); pp e2])
-  | Lift (d1, d2, e) ->
+  | Match (a, e, cases) ->
+    nest (!^ "Match" ^^ Pp.list [pp_a a; pp e;
+      cases |> OCaml.list (fun (p, e) ->
+        nest @@ parens (Pattern.pp p ^-^ !^ "," ^^ pp e))])
+  | Record (a, fields) ->
+    nest (!^ "Record" ^^ Pp.list (pp_a a :: (fields |> List.map (fun (x, e) ->
+      nest @@ parens (PathName.pp x ^-^ !^ "," ^^ pp e)))))
+  | Field (a, e, x) ->
+    nest (!^ "Field" ^^ Pp.list [pp_a a; pp e; PathName.pp x])
+  | IfThenElse (a, e1, e2, e3) ->
+    nest (!^ "IfThenElse" ^^ Pp.list [pp_a a; pp e1; pp e2; pp e3])
+  | Sequence (a, e1, e2) ->
+    nest (!^ "Sequence" ^^ Pp.list [pp_a a; pp e1; pp e2])
+  | Return (a, e) -> nest (!^ "Return" ^^ Pp.list [pp_a a; pp e])
+  | Bind (a, e1, x, e2) -> nest (!^ "Bind" ^^ Pp.list
+    [pp_a a; pp e1; nest (OCaml.option Name.pp x); pp e2])
+  | Lift (a, d1, d2, e) ->
     nest (!^ "Lift" ^^
-      Pp.list [Effect.Descriptor.pp d1; Effect.Descriptor.pp d2; pp e])
+      Pp.list [pp_a a; Effect.Descriptor.pp d1; Effect.Descriptor.pp d2; pp e])
 
 (** Take a function expression and make explicit the list of arguments and
     the body. *)
-let rec open_function (e : t) : Name.t list * t =
+let rec open_function (e : 'a t) : Name.t list * 'a t =
   match e with
-  | Function (x, e) ->
+  | Function (_, x, e) ->
     let (xs, e) = open_function e in
     (x :: xs, e)
   | _ -> ([], e)
 
 (** Import an OCaml expression. *)
-let rec of_expression (e : expression) : t =
+let rec of_expression (e : expression) : unit Env.t t =
   match e.exp_desc with
-  | Texp_ident (path, _, _) -> Variable (PathName.of_path path)
-  | Texp_constant constant -> Constant (Constant.of_constant constant)
+  | Texp_ident (path, _, _) -> Variable ((), PathName.of_path path)
+  | Texp_constant constant -> Constant ((), Constant.of_constant constant)
   | Texp_let (rec_flag, [{ vb_pat = pattern; vb_expr = e1 }], e2) ->
     let (rec_flag, pattern, free_typ_vars, args, body_typ, body) =
       import_let_fun rec_flag pattern e1 in
     let e2 = of_expression e2 in
     (match (pattern, args) with
     | (Pattern.Variable name, []) ->
-      Let ((Recursivity.New false, name, [], [], None), body, e2)
-    | (_, []) -> Match (body, [pattern, e2])
+      Let ((), (Recursivity.New false, name, [], [], None), body, e2)
+    | (_, []) -> Match ((), body, [pattern, e2])
     | (Pattern.Variable name, _) ->
-      Let ((rec_flag, name, free_typ_vars, args, Some body_typ), body, e2)
+      Let ((), (rec_flag, name, free_typ_vars, args, Some body_typ), body, e2)
     | _ -> failwith "Cannot match a function definition on a pattern.")
   | Texp_function (_, [{c_lhs = {pat_desc = Tpat_var (x, _)}; c_rhs = e}], _)
   | Texp_function (_, [{c_lhs = { pat_desc = Tpat_alias
     ({ pat_desc = Tpat_any }, x, _)}; c_rhs = e}], _) ->
-    Function (Name.of_ident x, of_expression e)
+    Function ((), Name.of_ident x, of_expression e)
   | Texp_function (_, cases, _) ->
     let (x, e) = open_cases cases in
-    Function (x, e)
+    Function ((), x, e)
   | Texp_apply (e_f, e_xs) ->
     let e_f = of_expression e_f in
     let e_xs = List.map (fun (_, e_x, _) ->
@@ -112,36 +137,42 @@ let rec of_expression (e : expression) : t =
       | Some e_x -> of_expression e_x
       | None -> failwith "expected an argument") e_xs in
     (match (e_f, e_xs) with
-    | (Variable x, [Constructor (exn, es)])
+    | (Variable ((), x), [Constructor ((), exn, es)])
       when x = PathName.of_name ["Pervasives"] "raise" ->
-      Apply (Variable exn, Tuple es)
-    | _ -> List.fold_left (fun e e_x -> Apply (e, e_x)) e_f e_xs)
+      Apply ((), Variable ((), exn), Tuple ((), es))
+    | _ -> List.fold_left (fun e e_x -> Apply ((), e, e_x)) e_f e_xs)
   | Texp_match (e, cases, _) ->
     let e = of_expression e in
-    let cases = List.map (fun {c_lhs = p; c_rhs = e} -> (Pattern.of_pattern p, of_expression e)) cases in
-    Match (e, cases)
-  | Texp_tuple es -> Tuple (List.map of_expression es)
-  | Texp_construct (x, _, es) -> Constructor (PathName.of_loc x, List.map of_expression es)
-  | Texp_record (fields, _) -> Record (List.map (fun (x, _, e) -> (PathName.of_loc x, of_expression e)) fields)
-  | Texp_field (e, x, _) -> Field (of_expression e, PathName.of_loc x)
+    let cases = List.map (fun {c_lhs = p; c_rhs = e} ->
+      (Pattern.of_pattern p, of_expression e)) cases in
+    Match ((), e, cases)
+  | Texp_tuple es -> Tuple ((), List.map of_expression es)
+  | Texp_construct (x, _, es) ->
+    Constructor ((), PathName.of_loc x, List.map of_expression es)
+  | Texp_record (fields, _) ->
+    Record ((), fields |> List.map (fun (x, _, e) ->
+      (PathName.of_loc x, of_expression e)))
+  | Texp_field (e, x, _) -> Field ((), of_expression e, PathName.of_loc x)
   | Texp_ifthenelse (e1, e2, e3) ->
     let e3 = match e3 with
-      | None -> Constructor ({ PathName.path = []; base = "tt" }, [])
+      | None -> Constructor ((), { PathName.path = []; base = "tt" }, [])
       | Some e3 -> of_expression e3 in
-    IfThenElse (of_expression e1, of_expression e2, e3)
-  | Texp_sequence (e1, e2) -> Sequence (of_expression e1, of_expression e2)
+    IfThenElse ((), of_expression e1, of_expression e2, e3)
+  | Texp_sequence (e1, e2) -> Sequence ((), of_expression e1, of_expression e2)
   | Texp_try _ | Texp_setfield _ | Texp_array _
     | Texp_while _ | Texp_for _ | Texp_assert _ ->
     failwith "Imperative expression not handled."
   | _ -> failwith "Expression not handled."
 (** Generate a variable and a "match" on this variable from a list of patterns. *)
-and open_cases (cases : case list) : Name.t * t =
+and open_cases (cases : case list) : Name.t * unit t =
   let x = Name.unsafe_fresh "match_var" in
   let cases = cases |> List.map (fun {c_lhs = p; c_rhs = e} ->
     (Pattern.of_pattern p, of_expression e)) in
-  (x, Match (Variable (PathName.of_name [] x), cases))
-and import_let_fun (rec_flag : Asttypes.rec_flag) (pattern : pattern) (e : expression)
-  : Recursivity.t * Pattern.t * Name.t list * (Name.t * Type.t) list * Type.t * t =
+  (x, Match ((), Variable ((), PathName.of_name [] x), cases))
+and import_let_fun (rec_flag : Asttypes.rec_flag) (pattern : pattern)
+  (e : expression)
+  : Recursivity.t * Pattern.t * Name.t list * (Name.t * Type.t) list
+    * Type.t * unit t =
   let pattern = Pattern.of_pattern pattern in
   let e_schema = Schema.of_type (Type.of_type_expr e.exp_type) in
   let e = of_expression e in
@@ -151,28 +182,32 @@ and import_let_fun (rec_flag : Asttypes.rec_flag) (pattern : pattern) (e : expre
   (Recursivity.of_rec_flag rec_flag, pattern, e_schema.Schema.variables,
     List.combine args_names args_typs, e_body_typ, e_body)
 
-let rec substitute (x : PathName.t) (e' : t) (e : t) : t =
+let rec substitute (x : PathName.t) (e' : 'a t) (e : 'a t) : 'a t =
   match e with
   | Constant _ -> e
-  | Variable y -> if x = y then e' else Variable y
-  | Tuple es -> Tuple (List.map (substitute x e') es)
-  | Constructor (y, es) -> Constructor (y, List.map (substitute x e') es)
-  | Apply (e1, e2) -> Apply (substitute x e' e1, substitute x e' e2)
-  | Function (y, e) ->
+  | Variable (_, y) -> if x = y then e' else e
+  | Tuple (a, es) -> Tuple (a, List.map (substitute x e') es)
+  | Constructor (a, y, es) -> Constructor (a, y, List.map (substitute x e') es)
+  | Apply (a, e1, e2) -> Apply (a, substitute x e' e1, substitute x e' e2)
+  | Function (a, y, e) ->
     if PathName.of_name [] y = x then
-      Function (y, e)
+      Function (a, y, e)
     else
-      Function (y, substitute x e' e)
-  | Let ((is_rec, y, typ_args, args, typ), e1, e2) ->
+      Function (a, y, substitute x e' e)
+  | Let (a, (is_rec, y, typ_args, args, typ), e1, e2) ->
     let e1 =
       if (Recursivity.to_bool is_rec && PathName.of_name [] y = x)
         || List.exists (fun (y, _) -> PathName.of_name [] y = x) args then
         e1
       else
         substitute x e' e1 in
-    let e2 = if PathName.of_name [] y = x then e2 else substitute x e' e2 in
-    Let ((is_rec, y, typ_args, args, typ), e1, e2)
-  | Match (e, cases) ->
+    let e2 =
+      if PathName.of_name [] y = x then
+        e2
+      else
+        substitute x e' e2 in
+    Let (a, (is_rec, y, typ_args, args, typ), e1, e2)
+  | Match (a, e, cases) ->
     let e = substitute x e' e in
     let cases = cases |> List.map (fun (p, e) ->
       let ys = Pattern.free_variables p in
@@ -180,15 +215,16 @@ let rec substitute (x : PathName.t) (e' : t) (e : t) : t =
         (p, e)
       else
         (p, substitute x e' e)) in
-    Match (e, cases)
-  | Record fields ->
-    Record (List.map (fun (y, e) -> (y, substitute x e' e)) fields)
-  | Field (e, y) -> Field (substitute x e' e, y)
-  | IfThenElse (e1, e2, e3) ->
-    IfThenElse (substitute x e' e1, substitute x e' e2, substitute x e' e3)
-  | Sequence (e1, e2) -> Sequence (substitute x e' e1, substitute x e' e2)
-  | Return e -> Return (substitute x e' e)
-  | Bind (e1, y, e2) ->
+    Match (a, e, cases)
+  | Record (a, fields) ->
+    Record (a, List.map (fun (y, e) -> (y, substitute x e' e)) fields)
+  | Field (a, e, y) -> Field (a, substitute x e' e, y)
+  | IfThenElse (a, e1, e2, e3) ->
+    IfThenElse (a, substitute x e' e1, substitute x e' e2, substitute x e' e3)
+  | Sequence (a, e1, e2) ->
+    Sequence (a, substitute x e' e1, substitute x e' e2)
+  | Return (a, e) -> Return (a, substitute x e' e)
+  | Bind (a, e1, y, e2) ->
     let e1 = substitute x e' e1 in
     let e2 =
       match y with
@@ -198,36 +234,39 @@ let rec substitute (x : PathName.t) (e' : t) (e : t) : t =
           e2
         else
           substitute x e' e2 in
-    Bind (e1, y, e2)
-  | Lift (d1, d2, e) -> Lift (d1, d2, substitute x e' e)
+    Bind (a, e1, y, e2)
+  | Lift (a, d1, d2, e) -> Lift (a, d1, d2, substitute x e' e)
 
-let rec monadise_let_rec (e : t) : t =
+let rec monadise_let_rec (e : 'a t) : 'a t =
   match e with
   | Constant _ | Variable _ -> e
-  | Tuple es -> Tuple (List.map monadise_let_rec es)
-  | Constructor (x, es) ->
-    Constructor (x, List.map monadise_let_rec es)
-  | Apply (e1, e2) -> Apply (monadise_let_rec e1, monadise_let_rec e2)
-  | Function (x, e) -> Function (x, monadise_let_rec e)
-  | Let (header, e1, e2) ->
+  | Tuple (a, es) -> Tuple (a, List.map monadise_let_rec es)
+  | Constructor (a, x, es) ->
+    Constructor (a, x, List.map monadise_let_rec es)
+  | Apply (a, e1, e2) -> Apply (a, monadise_let_rec e1, monadise_let_rec e2)
+  | Function (a, x, e) -> Function (a, x, monadise_let_rec e)
+  | Let (a, header, e1, e2) ->
     let defs = monadise_let_rec_definition header e1 in
     let e2 = monadise_let_rec e2 in
-    List.fold_right (fun (header, e) e2 -> Let (header, e, e2)) defs e2
-  | Match (e, cases) ->
-    Match (monadise_let_rec e,
+    List.fold_right (fun (header, e) e2 -> Let (a, header, e, e2)) defs e2
+  | Match (a, e, cases) ->
+    Match (a, monadise_let_rec e,
       List.map (fun (p, e) -> (p, monadise_let_rec e)) cases)
-  | Record fields ->
-    Record (List.map (fun (x, e) -> (x, monadise_let_rec e)) fields)
-  | Field (e, x) -> Field (monadise_let_rec e, x)
-  | IfThenElse (e1, e2, e3) ->
-    IfThenElse (monadise_let_rec e1, monadise_let_rec e2, monadise_let_rec e3)
-  | Sequence (e1, e2) -> Sequence (monadise_let_rec e1, monadise_let_rec e2)
-  | Return e -> monadise_let_rec e
-  | Bind (e1, x, e2) -> Bind (monadise_let_rec e1, x, monadise_let_rec e2)
-  | Lift (d1, d2, e) -> Lift (d1, d2, monadise_let_rec e)
+  | Record (a, fields) ->
+    Record (a, List.map (fun (x, e) -> (x, monadise_let_rec e)) fields)
+  | Field (a, e, x) -> Field (a, monadise_let_rec e, x)
+  | IfThenElse (a, e1, e2, e3) ->
+    IfThenElse (a, monadise_let_rec e1, monadise_let_rec e2,
+      monadise_let_rec e3)
+  | Sequence (a, e1, e2) ->
+    Sequence (a, monadise_let_rec e1, monadise_let_rec e2)
+  | Return (a, e) -> monadise_let_rec e
+  | Bind (a, e1, x, e2) ->
+    Bind (a, monadise_let_rec e1, x, monadise_let_rec e2)
+  | Lift (a, d1, d2, e) -> Lift (a, d1, d2, monadise_let_rec e)
 
-and monadise_let_rec_definition (header : Header.t) (e : t)
-  : (Header.t * t) list =
+and monadise_let_rec_definition (header : Header.t) (e : 'a t)
+  : (Header.t * 'a t) list =
   let (is_rec, x, typ_vars, args, typ) = header in
   let e = monadise_let_rec e in
   if Recursivity.to_bool is_rec then
