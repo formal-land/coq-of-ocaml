9b25020a3fd3ebec8b095a29c798907d6b176cfa
Guillaume Claret <dev@clarus.me>
2014-04-15 15:57:22 +0200

    A test file for the modules with type specification

---
+++ tests/ex31.v
@@ -0,0 +1,12 @@
+Require Import CoqOfOCaml.
+
+Local Open Scope Z_scope.
+Import ListNotations.
+
+Module M.
+  Definition b : bool := false.
+  
+  Definition n : Z := 12.
+End M.
+
+Definition n : Z := Z.add M.n 2.
