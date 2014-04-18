9fd9ee6a882e74bb99ec6349e576d760ae3c7a3d
Guillaume Claret <dev@clarus.me>
2014-04-18 16:06:50 +0200

    Simpler JSON interface format

--- interface.ml
+++ interface.ml
@@ -26,52 +26,21 @@ module Shape = struct
     of_list (of_list PathName.of_json) json
 end
 
-module Declaration = struct
-  type t =
-    | Var of Name.t * Shape.t
-    | Typ of Name.t
-    | Descriptor of Name.t
-    | Constructor of Name.t
-    | Field of Name.t
-
-  let pp (d : t) : SmartPrint.t =
-    match d with
-    | Var (x, shape) ->
-      !^ "Var" ^^ OCaml.tuple [Name.pp x; Shape.pp shape]
-    | Typ x -> !^ "Typ" ^^ Name.pp x
-    | Descriptor x -> !^ "Descriptor" ^^ Name.pp x
-    | Constructor x -> !^ "Constructor" ^^ Name.pp x
-    | Field x -> !^ "Field" ^^ Name.pp x
-
-  let to_json (d : t) : json =
-    match d with
-    | Var (x, shape) ->
-      `List [`String "Var"; `List [Name.to_json x; Shape.to_json shape]]
-    | Typ x -> `List [`String "Typ"; Name.to_json x]
-    | Descriptor x -> `List [`String "Descriptor"; Name.to_json x]
-    | Constructor x -> `List [`String "Constructor"; Name.to_json x]
-    | Field x -> `List [`String "Field"; Name.to_json x]
-
-  let of_json (json : json) : t =
-    match json with
-    | `List [`String c; arg] ->
-      (match (c, arg) with
-      | ("Var", `List [x; shape]) -> Var (Name.of_json x, Shape.of_json shape)
-      | ("Typ", x) -> Typ (Name.of_json x)
-      | ("Descriptor", x) -> Descriptor (Name.of_json x)
-      | ("Constructor", x) -> Constructor (Name.of_json x)
-      | ("Field", x) -> Field (Name.of_json x)
-      | _ -> failwith "Unknown field.")
-    | _ -> failwith "List expected."
-end
-
 type t =
-  | Declaration of Declaration.t
+  | Var of Name.t * Shape.t
+  | Typ of Name.t
+  | Descriptor of Name.t
+  | Constructor of Name.t
+  | Field of Name.t
   | Interface of Name.t * t list
 
 let rec pp (interface : t) : SmartPrint.t =
   match interface with
-  | Declaration d -> Declaration.pp d
+  | Var (x, shape) -> !^ "Var" ^^ OCaml.tuple [Name.pp x; Shape.pp shape]
+  | Typ x -> !^ "Typ" ^^ Name.pp x
+  | Descriptor x -> !^ "Descriptor" ^^ Name.pp x
+  | Constructor x -> !^ "Constructor" ^^ Name.pp x
+  | Field x -> !^ "Field" ^^ Name.pp x
   | Interface (x, ds) ->
     !^ "Interface" ^^ Name.pp x ^^ !^ "=" ^^ newline ^^ indent
       (separate newline (List.map pp ds))
@@ -87,45 +56,50 @@ and of_structure (def : ('a * Effect.t) Structure.t) : t list =
       let typ =
         Effect.function_typ header.Exp.Header.args (snd (Exp.annotation e)) in
       (name, Shape.of_effect_typ @@ Effect.Type.compress typ)) in
-    values |> List.map (fun (name, typ) ->
-      Declaration (Declaration.Var (name, typ)))
+    values |> List.map (fun (name, typ) -> Var (name, typ))
   | Structure.Inductive (_, ind) ->
     let name = ind.Structure.Inductive.name in
     let constructors = List.map fst ind.Structure.Inductive.constructors in
-    Declaration (Declaration.Typ name) :: List.map (fun x ->
-      Declaration (Declaration.Constructor x)) constructors
+    Typ name :: List.map (fun x -> Constructor x) constructors
   | Structure.Record (_, r) ->
     let name = r.Structure.Record.name in
     let fields = List.map fst r.Structure.Record.fields in
-    Declaration (Declaration.Typ name) :: List.map (fun x ->
-      Declaration (Declaration.Field x)) fields
-  | Structure.Synonym (_, s) ->
-    [Declaration (Declaration.Typ s.Structure.Synonym.name)]
+    Typ name :: List.map (fun x -> Field x) fields
+  | Structure.Synonym (_, s) -> [Typ s.Structure.Synonym.name]
   | Structure.Exception (_, exn) ->
     let name = exn.Structure.Exception.name in
-    [ Declaration (Declaration.Descriptor name);
-      Declaration (Declaration.Var (name, [[PathName.of_name [] name]])) ]
+    [ Descriptor name; Var (name, [[PathName.of_name [] name]]) ]
   | Structure.Reference (_, r) ->
     let name = r.Structure.Reference.name in
     let shape = [[PathName.of_name [] name]] in
-    [ Declaration (Declaration.Descriptor name);
-      Declaration (Declaration.Var ("read_" ^ name, shape));
-      Declaration (Declaration.Var ("write_" ^ name, shape)) ]
+    [ Descriptor name;
+      Var ("read_" ^ name, shape);
+      Var ("write_" ^ name, shape) ]
   | Structure.Open _ -> []
   | Structure.Module (_, name, defs) -> [Interface (name, of_structures defs)]
 
 let rec to_json (interface : t) : json =
   match interface with
-  | Declaration d -> `List [`String "Declaration"; Declaration.to_json d]
+  | Var (x, shape) ->
+    `List [`String "Var"; `List [Name.to_json x; Shape.to_json shape]]
+  | Typ x -> `List [`String "Typ"; Name.to_json x]
+  | Descriptor x -> `List [`String "Descriptor"; Name.to_json x]
+  | Constructor x -> `List [`String "Constructor"; Name.to_json x]
+  | Field x -> `List [`String "Field"; Name.to_json x]
   | Interface (x, ds) ->
     `List [`String "Interface";
       `List [Name.to_json x; `List (List.map to_json ds)]]
 
 let rec of_json (json : json) : t =
   match json with
-  | `List [`String "Declaration"; d] -> Declaration (Declaration.of_json d)
+  | `List [`String "Var"; `List [x; shape]] ->
+    Var (Name.of_json x, Shape.of_json shape)
+  | `List [`String "Typ"; x] -> Typ (Name.of_json x)
+  | `List [`String "Descriptor"; x] -> Descriptor (Name.of_json x)
+  | `List [`String "Constructor"; x] -> Constructor (Name.of_json x)
+  | `List [`String "Field"; x] -> Field (Name.of_json x)
   | `List [`String "Interface"; `List [x; `List ds]] ->
-    Interface (Name.of_json x, List.map of_json ds)
+      Interface (Name.of_json x, List.map of_json ds)
   | _ -> failwith "Wrong JSON format."
 
 let to_json_string (interface : t) : string =
