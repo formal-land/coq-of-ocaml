4d8a847db0e09ddbd63a222ce2e4619d867cc277
Guillaume Claret <dev@clarus.me>
2019-08-20 11:56:25 +0200

    Do not duplicate file name in locations

--- src/loc.ml
+++ src/loc.ml
@@ -3,24 +3,25 @@ open SmartPrint
 
 module Position = struct
   type t = {
-    file_name : string;
     line : int }
     [@@deriving sexp]
 
   let of_position (position : Lexing.position) : t =
-    { file_name = position.Lexing.pos_fname ; line = position.Lexing.pos_lnum }
+    { line = position.Lexing.pos_lnum }
 end
 
 type t =
   | Unknown
-  | Known of Position.t * Position.t
+  | Known of string * Position.t * Position.t
   [@@deriving sexp]
 
 let to_user (l : t) : SmartPrint.t =
   match l with
   | Unknown -> !^ "?"
-  | Known ({ file_name; line }, _) ->
+  | Known (file_name, { line }, _) ->
     !^ file_name ^-^ !^ "," ^^ !^ "line" ^^ OCaml.int line
 
 let of_location (l : Location.t) : t =
-  Known (Position.of_position l.Location.loc_start, Position.of_position l.Location.loc_end)
+  let start = l.Location.loc_start in
+  let _end = l.Location.loc_end in
+  Known (start.Lexing.pos_fname, Position.of_position start, Position.of_position _end)
