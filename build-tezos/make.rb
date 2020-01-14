3172a606649244363f64f39e6d660a46aa2b11f4
Guillaume Claret <dev@clarus.me>
2020-01-15 00:21:18 +0100

    Add scripts to compile and compute the number of valid lines of the generated Coq

---
+++ build-tezos/make.rb
@@ -0,0 +1,33 @@
+require 'open3'
+
+tezos_folder = "../../../Tezos/gitlab/tezos"
+protocol_folder = "#{tezos_folder}/src/proto_alpha/lib_protocol"
+interface_file = "#{tezos_folder}/generated/Environment_mli.v"
+environment_file = "environment.v"
+
+system("cp #{interface_file} #{environment_file}")
+system("cp #{protocol_folder}/*.v ./")
+
+files = [
+  environment_file,
+  "Raw_level_repr.v"
+]
+
+nb_valid_lines = 0
+
+for file in files do
+  command = "coqc -R . Tezos #{file}"
+  puts command
+  output, status = Open3.capture2e(command)
+  if status.to_i == 0 then
+    nb_lines = File.read(file).split("\n").size
+    puts nb_lines
+    nb_valid_lines += nb_lines if file != environment_file
+  else
+    puts output
+    line_number = /line (\d+)/.match(output)[1]
+    nb_valid_lines += line_number.to_i - 1 if file != environment_file
+  end
+end
+
+puts "Total number of valid lines: #{nb_valid_lines}"
