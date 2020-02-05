6eefebc648af62925e6af5570c4d4828187c6ac6
Guillaume Claret <dev@clarus.me>
2020-02-05 14:30:45 +0100

    Use warnings instead of errors

--- examples/make.rb
+++ examples/make.rb
@@ -38,7 +38,7 @@ def mark_text(text, errors)
     if fragment[:errors].size == 0 then
       output << encoded_text
     else
-      output << "<abbr class=\"mark-error\" title=\"#{encoded_errors}\">#{encoded_text}</abbr>"
+      output << "<abbr class=\"mark-warning\" title=\"#{encoded_errors}\">#{encoded_text}</abbr>"
     end
   end
 
