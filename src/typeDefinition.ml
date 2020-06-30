13236afbee763ebdb725e05d37b7c791ce0f519a
Pedro Abreu <abreu223@hotmail.com>
2020-06-30 15:10:58 -0400

    Extract Constructors to their own file
    This is important to extract tags to their own file too

--- src/typeDefinition.ml
+++ src/typeDefinition.ml
@@ -2,250 +2,9 @@ open Typedtree
 open SmartPrint
 open Monad.Notations
 
-let to_coq_record
-  (module_name : Name.t)
-  (typ_name : Name.t)
-  (typ_args : Name.t list)
-  (fields : (Name.t * Type.t) list)
-  (with_with : bool)
-  : SmartPrint.t =
-  nest (
-    !^ "Module" ^^ Name.to_coq module_name ^-^ !^ "." ^^ newline ^^
-    indent (
-      !^ "Record" ^^ !^ "record" ^^
-      begin match typ_args with
-      | [] -> empty
-      | _ :: _ ->
-        braces (nest (
-          separate space (List.map Name.to_coq typ_args) ^^
-          !^ ":" ^^ Pp.set
-        ))
-      end ^^
-      nest (!^ ":" ^^ Pp.set) ^^
-      !^ ":=" ^^ !^ "Build" ^^
-      !^ "{" ^^ newline ^^
-      indent (separate (!^ ";" ^^ newline) (fields |> List.map (fun (x, typ) ->
-        nest (Name.to_coq x ^^ !^ ":" ^^ Type.to_coq None None typ)))) ^^
-      !^ "}." ^^
-      begin match typ_args with
-      | [] -> empty
-      | _ :: _ ->
-        newline ^^ !^ "Arguments" ^^ !^ "record" ^^ !^ ":" ^^
-        !^ "clear" ^^ !^ "implicits" ^-^ !^ "."
-      end ^^
-      newline ^^
-      begin if with_with then
-        separate newline (fields |> List.map (fun (name, _) ->
-          let prefixed_typ_args =
-            typ_args |> List.map (fun typ_arg ->
-              Name.to_coq (Name.prefix_by_t typ_arg)
-              ) in
-          let record_typ =
-            nest (separate space (!^ "record" :: prefixed_typ_args)) in
-          nest (
-            !^ "Definition" ^^ Name.to_coq (Name.prefix_by_with name) ^^
-            begin match typ_args with
-            | [] -> empty
-            | _ :: _ -> braces (nest (separate space prefixed_typ_args))
-            end ^^
-            Name.to_coq name ^^
-            nest (parens (!^ "r" ^^ !^ ":" ^^ record_typ)) ^-^
-            !^ " :=" ^^ newline ^^
-            indent @@ nest (
-              !^ "Build" ^^
-              separate space prefixed_typ_args ^^
-              separate space (fields |> List.map (fun (name', _) ->
-                nest (
-                  if Name.equal name name' then
-                    Name.to_coq name
-                  else
-                  !^ "r" ^-^ !^ ".(" ^-^ Name.to_coq name' ^-^ !^ ")"
-                )
-              )) ^-^ !^ "."
-            )
-          )
-        )) ^^ newline
-      else
-        empty
-      end
-    ) ^^
-    !^ "End" ^^ Name.to_coq module_name ^-^ !^ "." ^^ newline ^^
-    nest (
-      !^ "Definition" ^^ Name.to_coq typ_name ^^ !^ ":=" ^^
-      Name.to_coq module_name ^-^ !^ ".record" ^-^ !^ "."
-    )
-  )
-
-module RecordSkeleton = struct
-  type t = {
-    fields : Name.t list;
-    module_name : Name.t;
-    typ_name : Name.t;
-  }
-
-  let to_coq (record_skeleton : t) : SmartPrint.t =
-    let { fields; module_name; typ_name } = record_skeleton in
-    to_coq_record module_name typ_name fields (fields |>
-      List.map (fun field -> (field, Type.Variable field))
-    ) true
-end
-
-(** The constructors of an inductive type, either in a GADT or non-GADT form. *)
-module Constructors = struct
-  (** [constructor_name]: forall [typ_vars], [param_typs] -> t [res_typ_params] *)
-  type item = {
-    constructor_name : Name.t;
-    param_typs : Type.t list; (** The parameters of the constructor. *)
-    res_typ_params : Type.t list;
-      (** The type parameters of the result type of the constructor. *)
-    typ_vars : Name.t list; (** The polymorphic type variables. *)
-  }
-
-  type t = item list
-
-  module Single = struct
-    type t = {
-      constructor_name : Name.t;
-      param_typs : Type.t list; (** The parameters of the constructor. *)
-      return_typ_params : Type.t list;
-        (** The return type, in case of GADT constructor, with some inference to
-            rule-out GADTs with only existential variables. *)
-    }
-
-    let of_ocaml_case
-      (typ_name : Name.t)
-      (defined_typ_params : Name.t list)
-      (case : Types.constructor_declaration)
-      : (t * (RecordSkeleton.t * Name.t list * Type.t) option) Monad.t =
-      let { Types.cd_args; cd_id; cd_loc; cd_res; _ } = case in
-      set_loc (Loc.of_location cd_loc) (
-      let* constructor_name =
-        PathName.map_constructor_name
-          (Ident.name cd_id) (Name.to_string typ_name) in
-      let* constructor_name = Name.of_string false constructor_name in
-      let typ_vars = Name.Map.empty in
-      begin match cd_args with
-      | Cstr_tuple param_typs ->
-        Type.of_typs_exprs true typ_vars param_typs >>= fun (param_typs, _, _) ->
-        return (param_typs, None)
-      | Cstr_record labeled_typs ->
-        set_loc (Loc.of_location cd_loc) (
-        (
-          labeled_typs |>
-          List.map (fun { Types.ld_type; _ } -> ld_type) |>
-          Type.of_typs_exprs true typ_vars
-        ) >>= fun (record_params, _, _) ->
-        let* record_fields =
-          labeled_typs |> Monad.List.map ( fun { Types.ld_id; _ } ->
-            Name.of_ident false ld_id
-          ) in
-        (* We get the order of the type arguments from their order of
-          introduction in the record fields. *)
-        let typ_args =
-          List.fold_left
-            (fun typ_args new_typ_args ->
-               (typ_args @
-               Name.Set.elements (
-                 Name.Set.diff new_typ_args (Name.Set.of_list typ_args)
-               ))
-            )
-            []
-            (List.map Type.typ_args_of_typ record_params) in
-        return (
-          [
-            Type.Apply (
-              MixedPath.PathName {
-                path = [typ_name];
-                base = constructor_name;
-              },
-              typ_args |> List.map (fun name ->
-                Type.Variable name
-              )
-            )
-          ],
-          Some (
-            {
-              RecordSkeleton.fields = record_fields;
-              module_name = constructor_name;
-              typ_name = Name.suffix_by_skeleton constructor_name;
-            },
-            typ_args,
-            Type.Apply (
-              MixedPath.PathName {
-                path = [typ_name];
-                base = Name.suffix_by_skeleton constructor_name;
-              },
-              record_params
-            )
-          )
-        ))
-      end >>= fun (param_typs, records) ->
-      let* return_typ_params =
-        match cd_res with
-        | Some typ -> Type.of_typ_expr false Name.Map.empty typ >>= fun (ty, _, _) ->
-          begin match ty with
-          | Type.Apply (_, typs) -> return typs
-          | _ -> raise [ty] Error.Category.Unexpected "Unexpected Type of record"
-          end
-        | None -> return (List.map (fun v -> Type.Variable v) defined_typ_params)
-      in
-
-      return (
-        {
-          constructor_name;
-          param_typs;
-          return_typ_params;
-        },
-        records
-      ))
-
-    let of_ocaml_row
-      (defined_typ_params : AdtParameters.t)
-      (row : Asttypes.label * Types.row_field)
-      : t Monad.t =
-      let (label, field) = row in
-      let* constructor_name = Name.of_string false label in
-      let typs = Type.type_exprs_of_row_field field in
-      Type.of_typs_exprs true Name.Map.empty typs >>= fun (param_typs, _, _) ->
-      return {
-        constructor_name;
-        param_typs;
-        return_typ_params = List.map (fun x -> Type.Variable x) (AdtParameters.get_parameters defined_typ_params);
-      }
-  end
-
-  let of_ocaml
-    (single_constructors : Single.t list)
-    : (t * AdtParameters.t) Monad.t =
-
-    let* constructors_and_arities  = single_constructors |> Monad.List.map (
-      fun { Single.constructor_name; param_typs; return_typ_params } ->
-        let typ_vars = param_typs |> Type.typ_args_of_typs in
-        let typ_vars = return_typ_params |> Type.typ_args_of_typs |> Name.Set.union typ_vars |> Name.Set.elements in
-          return ({
-            constructor_name;
-            param_typs;
-            res_typ_params = return_typ_params;
-            typ_vars
-          }, List.length return_typ_params)
-    ) in
-
-    let (constructors, constructors_arity) = List.split constructors_and_arities in
-    let same_arities = List.for_all (fun y -> List.hd constructors_arity = y) constructors_arity in
-
-    if not same_arities
-    then
-      raise (constructors, [])
-        Error.Category.Unexpected
-        "Unexpected error made the constructors have different return sizes"
-    else
-      return (constructors, [])
-
-end
-
 module Tags = struct
   (* We keep the actual types around for the decode function later on *)
-  type t = Name.t * Type.t list * Constructors.t
+  type t = Name.t * Type.t list * AdtConstructors.t
 
   let get_tag_constructor
       (name : Name.t)
@@ -261,7 +20,7 @@ module Tags = struct
     let constructors = typs |> List.fold_left (fun constructors typ ->
         let typ_vars = Type.typ_args_of_typ typ |> Name.Set.elements in
         let constructor_name = get_tag_constructor name typ in
-        let constructor : Constructors.item = {
+        let constructor : AdtConstructors.item = {
           constructor_name;
           param_typs = [];
           res_typ_params = [];
@@ -277,16 +36,16 @@ module Inductive = struct
 
   type t = {
     constructor_records
-      : (Name.t * (RecordSkeleton.t * Name.t list * Type.t) list) list;
+      : (Name.t * (AdtConstructors.RecordSkeleton.t * Name.t list * Type.t) list) list;
     notations : notation list;
-    records : RecordSkeleton.t list;
+    records : AdtConstructors.RecordSkeleton.t list;
     (* typs is a list of mutually defined Inductives
      * of the form
      * Inductive `Name.t` (`Name.t list` : Set) : Set :=
      * | `Constructor.t` ...
      * with `Name.t` (`Name.t list` : set) : Set := ...
      *)
-    typs : (Name.t * Name.t list * Constructors.t) list;
+    typs : (Name.t * Name.t list * AdtConstructors.t) list;
   }
 
   let get_notation_module_name (inductive : t) : SmartPrint.t =
@@ -310,7 +69,7 @@ module Inductive = struct
               separate
                 (newline ^^ newline)
                 (records |> List.map (fun (record, _, _) ->
-                  RecordSkeleton.to_coq record
+                  AdtConstructors.RecordSkeleton.to_coq record
                 ))
             ) ^^ newline ^^
             !^ "End" ^^ Name.to_coq name ^-^ !^ "."
@@ -329,7 +88,7 @@ module Inductive = struct
   let to_coq_notations_reserved (inductive : t) : SmartPrint.t list =
     (inductive.constructor_records |> List.map
       (fun (name, records) ->
-        records |> List.map (fun ({ RecordSkeleton.module_name; _ }, _, _) ->
+        records |> List.map (fun ({ AdtConstructors.RecordSkeleton.module_name; _ }, _, _) ->
           !^ "Reserved Notation" ^^ to_coq_notation_record name module_name ^-^
           !^ "."
         )
@@ -341,7 +100,7 @@ module Inductive = struct
     ))
 
   let to_coq_record_skeletons (inductive : t) : SmartPrint.t list =
-    inductive.records |> List.map RecordSkeleton.to_coq
+    inductive.records |> List.map AdtConstructors.RecordSkeleton.to_coq
 
   let to_coq_notations_where
     (subst : Type.Subst.t)
@@ -401,7 +160,7 @@ module Inductive = struct
     )) @
     (inductive.constructor_records |> List.map
       (fun (name, records) ->
-        records |> List.map (fun ({ RecordSkeleton.module_name; _ }, typ_args, typ) ->
+        records |> List.map (fun ({ AdtConstructors.RecordSkeleton.module_name; _ }, typ_args, typ) ->
           to_coq_notations_where
             subst
             (to_coq_notation_record name module_name)
@@ -430,7 +189,7 @@ module Inductive = struct
             !^ "Include" ^^ notation_module_name ^-^ !^ "." ^-^
               Name.to_coq name ^-^ !^ "." ^^ newline ^^
             separate newline (records |> List.map
-              (fun ({ RecordSkeleton.module_name; _ }, _, _) ->
+              (fun ({ AdtConstructors.RecordSkeleton.module_name; _ }, _, _) ->
                 to_coq_notations_definition
                   module_name
                   (!^ "'" ^-^ Name.to_coq name ^-^ !^ "." ^-^ Name.to_coq module_name)
@@ -450,13 +209,13 @@ module Inductive = struct
 
   let to_coq_typs_implicits
     (params : Name.t list)
-    (constructors : Constructors.t)
+    (constructors : AdtConstructors.t)
     : SmartPrint.t list =
     match params with
     | [] -> []
     | _ :: _ ->
       constructors |> List.map (fun {
-          Constructors.constructor_name; typ_vars; _
+          AdtConstructors.constructor_name; typ_vars; _
         } ->
         !^ "Arguments" ^^ Name.to_coq constructor_name ^^ braces (
           nest (
@@ -487,9 +246,9 @@ let filter_in_free_vars
 
 let tag_constructor
     (name : Name.t)
-    (constructor : Constructors.item)
-    : Constructors.item =
-  let { Constructors.res_typ_params; typ_vars; _ } = constructor in
+    (constructor : AdtConstructors.item)
+    : AdtConstructors.item =
+  let { AdtConstructors.res_typ_params; typ_vars; _ } = constructor in
   let typ_vars = typ_vars |> List.map (fun typ -> Type.Variable typ) in
   let res_typ_params = res_typ_params |> List.map
                          (fun res_typ_param -> Tags.get_tag_constructor name res_typ_param)
@@ -497,7 +256,7 @@ let tag_constructor
   { constructor with res_typ_params }
 
 let tag_constructors (name : Name.t)
-  : Constructors.t -> Constructors.t =
+  : AdtConstructors.t -> AdtConstructors.t =
   List.map (fun item -> tag_constructor name item)
 
 let of_ocaml (typs : type_declaration list) : t Monad.t =
@@ -509,9 +268,9 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
     begin match typ.Types.desc with
     | Tvariant { row_fields; _ } ->
       Monad.List.map
-        (Constructors.Single.of_ocaml_row ind_vars)
+        (AdtConstructors.Single.of_ocaml_row ind_vars)
         row_fields >>= fun single_constructors ->
-      Constructors.of_ocaml single_constructors >>= fun (constructors, _) ->
+      AdtConstructors.of_ocaml single_constructors >>= fun (constructors, _) ->
       raise
         (Inductive (None ,{
           constructor_records = [];
@@ -565,8 +324,8 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
       | { typ_type = { type_manifest = Some typ; _ }; _ } ->
         begin match typ.Types.desc with
         | Tvariant { row_fields; _ } ->
-          Monad.List.map (Constructors.Single.of_ocaml_row typ_args) row_fields >>= fun single_constructors ->
-          Constructors.of_ocaml single_constructors >>= fun (constructors, _) ->
+          Monad.List.map (AdtConstructors.Single.of_ocaml_row typ_args) row_fields >>= fun single_constructors ->
+          AdtConstructors.of_ocaml single_constructors >>= fun (constructors, _) ->
           raise
             (
               constructor_records,
@@ -616,7 +375,7 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
             )
           ) :: notations,
           {
-            RecordSkeleton.fields = fields |> List.map fst;
+            AdtConstructors.RecordSkeleton.fields = fields |> List.map fst;
             module_name = name;
             typ_name = Name.suffix_by_skeleton name;
            } :: records,
@@ -624,7 +383,7 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
         )
       | { typ_type = { type_kind = Type_variant cases; _ }; _ } ->
         let typ_args = AdtParameters.get_parameters typ_args in
-        Monad.List.map (Constructors.Single.of_ocaml_case name typ_args) cases >>= fun cases ->
+        Monad.List.map (AdtConstructors.Single.of_ocaml_case name typ_args) cases >>= fun cases ->
         let (single_constructors, new_constructor_records) = List.split cases in
         let new_constructor_records =
           new_constructor_records |> Util.List.filter_map (fun x -> x) in
@@ -633,7 +392,7 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
           | [] -> constructor_records
           | _ :: _ ->
             (name, new_constructor_records) :: constructor_records in
-        Constructors.of_ocaml
+        AdtConstructors.of_ocaml
           single_constructors >>= fun (constructors, merged_typ_params) ->
         let typ_args = merged_typ_params in
         return (
@@ -651,7 +410,7 @@ let of_ocaml (typs : type_declaration list) : t Monad.t =
     ) ([], [], [], [])) >>= fun (constructor_records, notations, records, typs) ->
 
     let ret_typs = typs |> List.map (function (_, _, constructors) ->
-        constructors |> List.map (function constructor -> constructor.Constructors.res_typ_params)
+        constructors |> List.map (function constructor -> constructor.AdtConstructors.res_typ_params)
         |> List.flatten) |> List.flatten in
 
     let typs = typs |> List.map (function (name, typ_args, constructors) ->
@@ -678,7 +437,7 @@ let to_coq_typs
     (is_first : bool)
     (name : Name.t)
     (params : Name.t list)
-    (constructors : Constructors.t)
+    (constructors : AdtConstructors.t)
   : SmartPrint.t =
   let keyword = if is_first then !^ "Inductive" else !^ "with" in
   nest (
@@ -705,7 +464,7 @@ let to_coq_typs
     ^^ !^ ":=" ^-^
     separate empty (
       constructors |> List.map (fun {
-          Constructors.constructor_name;
+          AdtConstructors.constructor_name;
           param_typs;
           res_typ_params;
           typ_vars;
@@ -807,7 +566,7 @@ let to_coq (def : t) : SmartPrint.t =
           let use_notation =
             inductive.constructor_records |> List.exists (fun (name, records) ->
               records |> List.exists
-                (fun ({ RecordSkeleton.module_name; _ }, _, _) ->
+                (fun ({ AdtConstructors.RecordSkeleton.module_name; _ }, _, _) ->
                   name = prefix && module_name = base
                 )
             ) in
@@ -823,7 +582,7 @@ let to_coq (def : t) : SmartPrint.t =
       ) ^^
     to_coq_inductive subst inductive
   | Record (name, typ_args, fields, with_with) ->
-    to_coq_record name name typ_args fields with_with
+    AdtConstructors.RecordSkeleton.to_coq_record name name typ_args fields with_with
   | Synonym (name, typ_args, value) ->
     nest (
       !^ "Definition" ^^ Name.to_coq name ^^
