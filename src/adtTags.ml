70815afc9d929e89707e33ad45116edcb13772e4
Pedro Abreu <abreu223@hotmail.com>
2020-07-01 17:12:49 -0400

    Generates tag for variables used in type constructors

--- src/adtTags.ml
+++ src/adtTags.ml
@@ -14,8 +14,8 @@ let get_tag_constructor
 let of_typs
     (name : Name.t)
     (typs : Type.t list) : t =
-  let typs = typs |> List.sort_uniq Type.compare
-  in
+  let typs = typs |> List.sort_uniq compare |> List.sort_uniq Type.compare in
+  (* let typs = typs |> List.sort_uniq Type.compare in *)
   let constructors = typs |> List.fold_left (fun constructors typ ->
       let typ_vars = Type.typ_args_of_typ typ |> Name.Set.elements in
       let typ_name = Name.Make (Type.to_string typ) in
@@ -69,7 +69,29 @@ let tag_constructor
   let res_typ_params = res_typ_params |> List.map (fun typ -> tag_typ name typ) in
   { constructor with res_typ_params; param_typs }
 
-
 let tag_constructors (name : Name.t)
   : AdtConstructors.t -> AdtConstructors.t =
   List.map (fun item -> tag_constructor name item)
+
+let get_application
+    (name : Name.t)
+  : Type.t -> Type.t list = function
+  | Type.Apply (s, ts) ->
+    if s = MixedPath.of_name name
+    then ts
+    else []
+  | _ -> []
+
+let get_index_typs
+  (name : Name.t)
+  (constructors : AdtConstructors.t list)
+  : Type.t list =
+  let res_typ_params = constructors |> List.flatten
+                       |> List.map (fun { AdtConstructors.res_typ_params; _} -> res_typ_params )
+                       |> List.flatten in
+  let param_typs = constructors |> List.flatten
+                   |> List.map (fun { AdtConstructors.param_typs; _ } ->
+                       List.map (get_application name) param_typs)
+                   |> List.flatten
+                   |> List.flatten in
+  res_typ_params @ param_typs
