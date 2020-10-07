2e27bb505c1150de65af810474ff80d9a76c998b
Guillaume Claret <dev@clarus.me>
2020-10-07 14:13:36 +0200

    Handle mixed paths with sub-signatures

--- src/mixedPath.ml
+++ src/mixedPath.ml
@@ -12,7 +12,7 @@ let of_name (name : Name.t) : t =
   PathName (PathName.of_name [] name)
 
 let get_signature_path (path : Path.t) : Path.t option Monad.t =
-  get_env >>= fun env ->
+  let* env = get_env in
   match Env.find_module path env with
   | module_declaration ->
     let { Types.md_type; _ } = module_declaration in
@@ -24,30 +24,121 @@ let get_signature_path (path : Path.t) : Path.t option Monad.t =
     end
   | exception _ -> return None
 
-let rec of_path_aux (path : Path.t)
-  : (Path.t * (Path.t * string) list * Path.t option) Monad.t =
-  let* signature_path = get_signature_path path in
-  begin match path with
-  | Papply _ -> failwith "Unexpected path application"
-  | Pdot (path', field_string) ->
-    of_path_aux path' >>= fun (namespace_path, fields, signature_path) ->
-    begin match signature_path with
-    | Some signature_path ->
-      return (
-        namespace_path,
-        (signature_path, field_string) :: fields
+module SignedPath = struct
+  type t = {
+    path : Path.t;
+    signature_path : Path.t option;
+  }
+
+  let get (path : Path.t) : t Monad.t =
+    let* signature_path = get_signature_path path in
+    return { path; signature_path }
+end
+
+module RawDecomposedPath = struct
+  type t = SignedPath.t * (SignedPath.t * string) list
+
+  let rec get_rev (path : Path.t) : t Monad.t =
+    let* signed_path = SignedPath.get path in
+    match path with
+    | Papply _ -> failwith "Unexpected functor path application"
+    | Pident _ -> return (signed_path, [])
+    | Pdot (path', field) ->
+      let* (start_path, fields) = get_rev path' in
+      return (start_path, (signed_path, field) :: fields)
+end
+
+module DecomposedPath = struct
+  type t =
+    | WithField of SignedPath.t * string * t
+    | Final of SignedPath.t
+
+  let rec get_of_raw_decomposed_path_rev
+    (raw_decomposed_path_rev : RawDecomposedPath.t)
+    (decomposed_path : SignedPath.t -> t)
+    : t =
+    let (start_path, fields) = raw_decomposed_path_rev in
+    match fields with
+    | [] -> decomposed_path start_path
+    | (signed_path, field) :: fields ->
+      get_of_raw_decomposed_path_rev
+        (start_path, fields)
+        (fun next_path ->
+          WithField (next_path, field, decomposed_path signed_path)
+        )
+
+  let get (path : Path.t) : t Monad.t =
+    let* raw_decomposed_path_rev = RawDecomposedPath.get_rev path in
+    return (get_of_raw_decomposed_path_rev
+      raw_decomposed_path_rev
+      (fun next_path -> Final next_path))
+end
+
+module FlattenedDecomposedPath = struct
+  type t =
+    | WithField of Path.t * Path.t * string list * t
+    | Final of SignedPath.t
+
+  let rec get
+    (decomposed_path : DecomposedPath.t)
+    (current_signed_path : (Path.t * Path.t * string list) option)
+    : t =
+    match (decomposed_path, current_signed_path) with
+    | (
+        WithField ({ path; signature_path }, field, decomposed_path),
+        None
+      ) ->
+      begin match signature_path with
+      | None ->
+        get decomposed_path current_signed_path
+      | Some signature_path ->
+        let current_signed_path = Some (path, signature_path, [field]) in
+        get decomposed_path current_signed_path
+      end
+    | (
+        WithField ({ path; signature_path }, field, decomposed_path),
+        Some (current_path, current_signature_path, current_fields)
+      ) ->
+      begin match signature_path with
+      | None ->
+        let current_signed_path =
+          Some (
+            current_path, current_signature_path, current_fields @ [field]
+          ) in
+        get decomposed_path current_signed_path
+      | Some signature_path ->
+        let current_signed_path = Some (path, signature_path, [field]) in
+        WithField (
+          current_path, current_signature_path, current_fields,
+          get decomposed_path current_signed_path
+        )
+      end
+    | (
+        Final signed_path,
+        None
+      ) -> Final signed_path
+    | (
+        Final signed_path,
+        Some (current_path, current_signature_path, current_fields)
+      ) ->
+      WithField (
+        current_path, current_signature_path, current_fields,
+        Final signed_path
       )
-    | None ->
-      (* The sub-module of a first-class module is a first-class module too. *)
-      return (path, [])
-    end
-  | Pident _ -> return (path, [])
-  end >>= fun (path, fields) ->
-  return (path, fields, signature_path)
+end
+
+let rec of_path_aux (flattened_decomposed_path : FlattenedDecomposedPath.t)
+  : Path.t * (Path.t * string list) list * Path.t option =
+  match flattened_decomposed_path with
+  | WithField (path, signature_path, fields, flattened_decomposed_path) ->
+    let (path', fields', signature_path') =
+      of_path_aux flattened_decomposed_path in
+    (path, (signature_path, fields) :: fields', Some signature_path)
+  | Final { path; signature_path } -> (path, [], signature_path)
 
 (** If the module was declared in the current unbundled signature definition. *)
 let is_module_path_local (path : Path.t) : bool Monad.t =
-  get_env_stack >>= fun env_stack ->
+  let* env_stack = get_env_stack in
   let envs_without_path = env_stack |> List.filter (fun env ->
     match Env.find_module path env with
     | _ -> false
@@ -62,28 +153,35 @@ let of_path
   (path : Path.t)
   (long_ident : Longident.t option)
   : t Monad.t =
-  of_path_aux path >>= fun (base_path, fields, signature_path) ->
+  let* decomposed_path = DecomposedPath.get path in
+  let flattened_decomposed_path =
+    FlattenedDecomposedPath.get decomposed_path None in
+  let (base_path, fields, signature_path) =
+    of_path_aux flattened_decomposed_path in
+  let fields = List.rev fields in
   match (fields, signature_path) with
   | ([], None) ->
-    PathName.of_path_without_convert is_value base_path >>= fun path_name ->
-    PathName.try_convert path_name >>= fun conversion ->
+    let* path_name = PathName.of_path_without_convert is_value base_path in
+    let* conversion = PathName.try_convert path_name in
     begin match conversion with
     | None ->
       begin match long_ident with
       | None -> return (PathName path_name)
       | Some long_ident ->
-        PathName.of_long_ident is_value long_ident >>= fun path_name ->
+        let* path_name = PathName.of_long_ident is_value long_ident in
         return (PathName path_name)
       end
     | Some path_name -> return (PathName path_name)
     end
   | _ ->
-    is_module_path_local base_path >>= fun is_local ->
-    PathName.of_path_with_convert is_value base_path >>= fun base_path_name ->
-    (fields |> Monad.List.map (fun (signature_path, field_string) ->
-      Name.of_string is_value field_string >>= fun field_name ->
-      PathName.of_path_and_name_with_convert signature_path field_name
-    )) >>= fun fields ->
+    let* is_local = is_module_path_local base_path in
+    let* base_path_name = PathName.of_path_with_convert is_value base_path in
+    let* fields =
+      fields |> Monad.List.map (fun (signature_path, fields) ->
+        let field = String.concat "_" fields in
+        let* field_name = Name.of_string is_value field in
+        PathName.of_path_and_name_with_convert signature_path field_name
+      ) in
     return (Access (base_path_name, List.rev fields, is_local))
 
 let to_coq (path : t) : SmartPrint.t =
