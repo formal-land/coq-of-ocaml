d43ff307fa6c82137e19ebf368caeedc4ac87bb5
Pedro Abreu <abreu223@hotmail.com>
2021-03-08 11:28:16 -0500

    Tide up Tags.v

--- proofs/Tags.v
+++ proofs/Tags.v
@@ -5,17 +5,15 @@ Inductive vtag : Type :=
 | constr_tag : string -> Set -> vtag
 | arrow_tag : vtag -> vtag -> vtag
 | tuple_tag : vtag -> vtag -> vtag
-(* | var_tag : forall (t0 : Type), vtag. *)
 .
 
 Fixpoint decode_vtag (tag : vtag) {struct tag}: Set :=
   match tag with
-          | @constr_tag s t => t
-          | arrow_tag t1 t2 => decode_vtag t1 -> decode_vtag t2
-          | tuple_tag t1 t2 =>
-            (decode_vtag t1) * (decode_vtag t2)
-          (* | var_tag t => t *)
-          end.
+  | constr_tag s t => t
+  | arrow_tag t1 t2 => decode_vtag t1 -> decode_vtag t2
+  | tuple_tag t1 t2 =>
+    (decode_vtag t1) * (decode_vtag t2)
+  end.
 
 Notation int_tag := (constr_tag "int" int).
 Notation string_tag := (constr_tag "string" string).
