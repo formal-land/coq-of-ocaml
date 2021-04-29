b58cffb1455e9887a5ad9388e1932e6129aeabb9
Pedro Abreu <abreu223@hotmail.com>
2021-04-29 10:17:10 -0400

    Minor bug on tagged return type

--- tests/gadts.ml
+++ tests/gadts.ml
@@ -20,7 +20,7 @@ type 'a term =
   | T_Int : int -> int term
   | T_String : string -> string term
   | T_Sum : int term * int term -> int term
-  | T_Pair : 'a term * 'b term -> ('a * 'b) term
+  (* | T_Pair : 'a term * 'b term -> ('a * 'b) term *)
 [@@coq_tag_gadt]
 
 let rec get_int (e : int term) : int =
@@ -28,6 +28,11 @@ let rec get_int (e : int term) : int =
   | T_Int n -> n
   | T_Sum (e1, e2) -> get_int e1 + get_int e2
   | _ -> .
+
+let rec interp : type a. a term -> a = function
+  | T_Int n -> n
+  | T_String s -> s
+  | T_Sum (s1, s2) -> interp s1 + interp s2
 
 
 type 'a one_case =
