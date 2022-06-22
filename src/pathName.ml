fb5f869c05ad1928d5b0288a725a1f1762af99fc
Guillaume Claret <dev@clarus.me>
2022-06-22 23:18:02 +0200

    Upgrade to OCaml 4.14

--- src/pathName.ml
+++ src/pathName.ml
@@ -162,39 +162,45 @@ let rec iterate_in_aliases (path : Path.t) (nb_args : int) : Path.t Monad.t =
   in
   let* env = get_env in
   match Env.find_type path env with
-  | { type_manifest = Some { desc = Tconstr (path', args, _); _ }; _ }
-    when List.length args = nb_args ->
-      if is_in_barrier_module path && not (is_in_barrier_module path') then
-        return path
-      else iterate_in_aliases path' nb_args
+  | { type_manifest = Some typ } -> (
+      match Types.get_desc typ with
+      | Tconstr (path', args, _) when List.length args = nb_args ->
+          if is_in_barrier_module path && not (is_in_barrier_module path') then
+            return path
+          else iterate_in_aliases path' nb_args
+      | _ -> return path)
   | _ -> return path
 
 let of_constructor_description
     (constructor_description : Types.constructor_description) : t Monad.t =
-  match constructor_description with
-  | { cstr_name; cstr_res = { desc = Tconstr (path, args, _); _ }; _ } ->
+  match Types.get_desc constructor_description.cstr_res with
+  | Tconstr (path, args, _) ->
       let* path = iterate_in_aliases path (List.length args) in
       let typ_ident = Path.head path in
       of_path_without_convert false path >>= fun { path; _ } ->
-      let* cstr_name = map_constructor_name cstr_name (Ident.name typ_ident) in
+      let* cstr_name =
+        map_constructor_name constructor_description.cstr_name
+          (Ident.name typ_ident)
+      in
       Name.of_string false cstr_name >>= fun base -> convert { path; base }
-  | { cstr_name; _ } ->
-      Name.of_string false cstr_name >>= fun cstr_name ->
+  | _ ->
+      Name.of_string false constructor_description.cstr_name
+      >>= fun cstr_name ->
       let path = of_name [] cstr_name in
       raise path Unexpected
         "Unexpected constructor description without a type constructor"
 
 let of_label_description (label_description : Types.label_description) :
     t Monad.t =
-  match label_description with
-  | { lbl_name; lbl_res = { desc = Tconstr (path, args, _); _ }; _ } ->
+  match Types.get_desc label_description.lbl_res with
+  | Tconstr (path, args, _) ->
       let* path = iterate_in_aliases path (List.length args) in
       of_path_without_convert false path >>= fun { path; base } ->
-      Name.of_string false lbl_name >>= fun lbl_name ->
+      Name.of_string false label_description.lbl_name >>= fun lbl_name ->
       let path_name = { path = path @ [ base ]; base = lbl_name } in
       convert path_name
-  | { lbl_name; _ } ->
-      Name.of_string false lbl_name >>= fun lbl_name ->
+  | _ ->
+      Name.of_string false label_description.lbl_name >>= fun lbl_name ->
       let path = of_name [] lbl_name in
       raise path Unexpected
         "Unexpected label description without a type constructor"
